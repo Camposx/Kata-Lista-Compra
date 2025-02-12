7f73da260c9bee1a0dbdd246e577a9b3dfce20d0
Camposx <alejandrocamposgon@gmail.com>
2025-02-12 10:39:04 +0100

    feat: add api routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ShopListController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/list', [ShopListController::class, 'index'])->name('apiHome');
+Route::post('/list', [ShopListController::class, 'store'])->name('apiStore');
+Route::put('/list/{id}', [ShopListController::class, 'update'])->name('apiUpdate');
+Route::delete('/list/{id}', [ShopListController::class, 'destroy'])->name('apiDestroy');
