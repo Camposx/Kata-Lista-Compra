c63bedf1097219a0c0c5ba142c67e3c9bfaf164f
Camposx <alejandrocamposgon@gmail.com>
2025-02-12 10:38:46 +0100

    feat: add CRUD to list table

---
+++ app/Http/Controllers/Api/ShopListController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\ShopList;
+use Illuminate\Http\Request;
+
+class ShopListController extends Controller
+{
+
+    public function index()
+    {
+        $shopList = ShopList::all();
+
+        return response()->json($shopList, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $shopList = ShopList::create([
+            'product' => $request->product,
+            'quantity' => $request->quantity,
+        ]);
+
+        $shopList->save();
+
+        return response()->json($shopList, 200);
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $shopList = ShopList::findOrFail($id);
+
+        $shopList->update([
+            'product' => $request->product,
+            'quantity' => $request->quantity,
+        ]);
+        
+        $shopList->save();
+
+        return response()->json($shopList, 200);
+    }
+
+    public function destroy(string $id)
+    {
+        $shopList = ShopList::findOrFail($id);
+        $shopList->delete();
+    }
+}
